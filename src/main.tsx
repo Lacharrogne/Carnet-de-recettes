7d30f05680ac0767720d56ff768a3b33b52c697b
Lacharrogne <maxime1.charron@epitech.eu>
2026-05-03 20:36:15 +0200

    enlever Vercel SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <Analytics />        <App />
+        <Analytics />
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
